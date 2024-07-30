665e8ff4e79dbf783b5b10ec13cb438818cedc95
Miguel <guelmorello@gmail.com>
2024-07-30 11:02:18 -0300

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,6 @@ import EditTask from "./pages/tasks/edit";
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <RefineKbarProvider>
           <AntdApp>
             <DevtoolsProvider>
